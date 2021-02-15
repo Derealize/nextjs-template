10681fe0af9b1ba4a620ad21a9bd5ef04fe5c7f1
zicjin <zicjin@gmail.com>
2021-02-15 10:53:12 +0800

    assets config

--- components/Utility-First.tsx
+++ components/Utility-First.tsx
@@ -1,8 +1,8 @@
 const UtilityFirst = () => {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div className="my-10 p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
       <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="/chat.svg" alt="ChitChat Logo" />
+        <img className="h-12 w-12" src="/assets/chat.svg" alt="ChitChat Logo" />
       </div>
       <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
