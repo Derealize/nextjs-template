49de62b2f4fe79fe96abac02e341bbf6d725b540
zicjin <zicjin@gmail.com>
2021-03-02 14:31:21 +0800

    add inject javascript

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+if (window.derealize && window.derealize.injectScript) {
+  const script = document.createElement("script");
+  script.setAttribute("src", window.derealize.injectScript);
+  document.body.appendChild(script);
+}
