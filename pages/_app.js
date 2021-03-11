9a6bfbef46e973a0b34e9c18c35d45a6bc607a16
Cheney Jin <zicjin@gmail.com>
2021-03-11 10:14:46 +0800

    inject script useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window === "undefined" || !window.derealize) return;
+
+    let script = document.querySelector("#derealize");
+    if (!script) {
+      script = document.createElement("script");
+      script.id = "derealize";
+      script.setAttribute("src", window.derealize.injectScript);
+      document.body.appendChild(script);
+    }
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
 
-if (window.derealize && window.derealize.injectScript) {
-  const script = document.createElement("script");
-  script.setAttribute("src", window.derealize.injectScript);
-  document.body.appendChild(script);
-}
+// note:
+// process.browser is deprecated and the recommended approach is 'typeof window === "undefined"'
+// A custom _document.js is not rendered client-side. componentDidMount (or any lifecycle method) will never trigger for client-side.
