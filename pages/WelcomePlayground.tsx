64c51337c720f51898c66e54e129390daa7b3f78
Elliot <elliotreborn@gmail.com>
2021-07-12 22:07:13 +0800

    temp

--- pages/WelcomePlayground.tsx
+++ pages/WelcomePlayground.tsx
@@ -2,7 +2,13 @@ export default () => {
   return (
     <div className="ground">
       <figure className="bg-gray-100 rounded-xl">
-        <img className="w-32 h-32" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
+        <img
+          className="object-cover w-32 h-32"
+          src="/sarah-dayan.jpg"
+          alt=""
+          width="384"
+          height="512"
+        />
         <div className="pt-6 space-y-4">
           <blockquote>
             <p className="text-lg">
